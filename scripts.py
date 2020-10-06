d17c5e503ab5c9f7c30168ca9202021d17d8405b
juan_mata <juan.mnaranjo@hotmail.com>
2020-10-06 22:50:14 +0200

    Data type exercises partially finished

--- scripts.py
+++ scripts.py
@@ -57,6 +57,68 @@ print(result)
 
 # 2. BASIC DATA TYPES
 
+# a. Find the Runner-Up Score
+
+n = 5
+arr = [2, 3, 6, 6, 5]
+
+arr.sort()
+print(list(set(arr))[-2])
+
+# b. List Comprehensions
+
+x = 2
+y = 2
+z = 2
+n = 2
+
+ls = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
+
+print(ls)
+
+# c. Nested Lists
+# TODO: nested list
+
+
+# d. Find the percentage
+# TODO: find percentage (rounding is not working...)
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    student_marks = {}
+    for _ in range(n):
+        line = raw_input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = raw_input()
+    print(student_marks[query_name])
+    student = sum(student_marks[query_name])/len(student_marks[query_name])
+    print(round(float(student), 2))
+
+# e. Lists
+# I have copied "s = raw_input().split()" from the solution (did not know how to access this input variables)
+
+N = int(raw_input())
+list_ = []
+for i in range(N):
+    s = raw_input().split()
+    if s[0] == 'insert':
+        eval('list_.' + str(s[0]) + '(' + s[1] + ',' + s[2] + ')')
+    elif s[0] == 'print':
+        print(list_)
+    elif (s[0] == 'append') | (s[0] == 'remove'):
+        eval('list_.' + str(s[0]) + '(' + s[1] + ')')
+    else:
+        eval('list_.' + str(s[0]) + '()')
+
+
+# f. Tuples
+
+n = 2
+integer_list = [1, 2]
+t = tuple(integer_list)
+print(hash(t))
 
 # 3. STRINGS
 
